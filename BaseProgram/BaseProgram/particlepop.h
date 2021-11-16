69ff8f34b07c57b08e16a0413fa9a81a5a37ee61
toshikitsutsui0909 <toshiki.0909.tsutsui@gmail.com>
2021-11-16 11:24:13 +0900

    パーティクルの追加

---
+++ BaseProgram/BaseProgram/particlepop.h
@@ -0,0 +1,46 @@
+#ifndef _PARTICLEPOP_H_
+#define _PARTICLEPOP_H_
+//=====================================================================
+//
+//	パーティクル処理[particlepop.h]
+//	Author : toshiki
+//
+//=====================================================================
+
+//=====================================================================
+// インクルードファイル
+//=====================================================================
+#include "particle_info.h"
+
+//=====================================================================
+// クラス定義
+//=====================================================================
+class CParticlePop : public CParticleInfo
+{
+public:
+	//==================
+	// メンバ関数
+	//==================
+	CParticlePop();
+	~CParticlePop();
+
+	static CParticlePop * Create(void);
+	HRESULT Init(void);
+	void Update(void);
+	void Uninit(void);
+	void Draw(void);
+
+private:
+	void SetParticle(void);		// パーティクルを出す処理
+	//==================
+	// メンバ変数
+	//==================
+	float m_fAngle;				// 角度
+	float m_fSpeed;				// 速度
+	int m_nLife;				// 体力
+	static float m_fSize;
+
+	D3DXVECTOR3 PlayerPos;		// プレイヤーの位置
+};
+
+#endif
