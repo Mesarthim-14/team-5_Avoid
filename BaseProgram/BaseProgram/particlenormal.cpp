4b5036df34e1a4d0958929942f795508f9447d9d
toshikitsutsui0909 <toshiki.0909.tsutsui@gmail.com>
2021-11-22 12:07:27 +0900

    パーティクル追加

---
+++ BaseProgram/BaseProgram/particlenormal.cpp
@@ -0,0 +1,116 @@
+//=====================================================================
+//
+//	パーティクル処理[particle.h]
+//	Author : toshiki
+//
+//=====================================================================
+
+//=====================================================================
+// インクルードファイル
+//=====================================================================
+#include "particlenormal.h"
+#include "manager.h"
+#include "renderer.h"
+#include "library.h"
+#include "texture.h"
+#include "resource_manager.h"
+
+//=====================================================================
+// マクロ定義
+//=====================================================================
+#define POS         (ZeroVector3)
+#define SIZE        (D3DXVECTOR3(50.0f, 50.0f, 50.0f))
+#define SPEED       (5.0f)
+#define RANDOM      (3.14f)
+#define LIFE        (60)
+#define COLOR       (D3DXCOLOR(1.0f,1.0f,1.0f,1.0f))
+
+//=====================================================================
+// コンストラクタ
+//=====================================================================
+CParticleNormal::CParticleNormal()
+{
+    m_fAngle = 0.0f;
+    m_fSpeed = 0.0f;
+}
+
+//=====================================================================
+// デストラクタ
+//=====================================================================
+CParticleNormal::~CParticleNormal()
+{
+
+}
+
+//=====================================================================
+// 生成処理
+//=====================================================================
+CParticleNormal * CParticleNormal::Create(void)
+{
+    CParticleNormal * pParticle = new CParticleNormal;
+
+    if (pParticle != nullptr)
+    {
+        pParticle->SetSceneInfo(POS, SIZE);
+
+        pParticle->Init();
+        return pParticle;
+    }
+    return nullptr;
+}
+
+//=====================================================================
+// 初期化処理
+//=====================================================================
+HRESULT CParticleNormal::Init(void)
+{
+    CParticleInfo::Init();
+    SetParticle();
+    return S_OK;
+}
+
+//=====================================================================
+// 更新処理
+//=====================================================================
+void CParticleNormal::Update(void)
+{
+    CParticleInfo::Update();
+}
+
+//=====================================================================
+// 終了処理
+//=====================================================================
+void CParticleNormal::Uninit(void)
+{
+    CParticleInfo::Uninit();
+}
+
+//=====================================================================
+// 描画処理
+//=====================================================================
+void CParticleNormal::Draw(void)
+{
+    CParticleInfo::Draw();
+}
+
+//=====================================================================
+// パーティクルを出す処理
+//=====================================================================
+void CParticleNormal::SetParticle(void)
+{
+    // テクスチャの設定
+    CTexture *pTexture = GET_TEXTURE_PTR;
+    BindTexture(pTexture->GetTexture(CTexture::TEXTURE_NUM_PARTICLE));
+    // スピードの値を設定
+    m_fSpeed = SPEED;
+    // パーティクルの出る角度の設定
+    m_fAngle = CLibrary::Random(RANDOM);
+    // 移動させるための処理
+    D3DXVECTOR3 move = D3DXVECTOR3(sinf(m_fAngle)*m_fSpeed, CLibrary::Random(4.0f, m_fSpeed), cosf(m_fAngle)*m_fSpeed);
+    // 移動情報を設定
+    SetMove(move);
+    // 色の設定
+    SetColor(COLOR);
+    // パーティクルが消える時間の設定
+    SetLife(LIFE);
+}
