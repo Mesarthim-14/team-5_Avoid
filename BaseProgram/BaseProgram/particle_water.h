a36507fa06f9293a3ffc0c731167d4ae035bcc08
toshikitsutsui0909 <toshiki.0909.tsutsui@gmail.com>
2021-12-20 10:55:33 +0900

    ジャンプエフェクト修正

---
+++ BaseProgram/BaseProgram/particle_water.h
@@ -0,0 +1,44 @@
+#ifndef _PARTICLE_WATER_H_
+#define _PARTICLE_WATER_H_
+//=====================================================================
+//
+//    水に落ちた時のエフェクト処理[particlepop.h]
+//    Author : toshiki
+//
+//=====================================================================
+
+//=====================================================================
+// インクルードファイル
+//=====================================================================
+#include "particle_info.h"
+
+//=====================================================================
+// クラス定義
+//=====================================================================
+class CParticleWater : public CParticleInfo
+{
+public:
+    //==================
+    // メンバ関数
+    //==================
+    CParticleWater();
+    ~CParticleWater();
+
+    static CParticleWater * Create(const D3DXVECTOR3 &pos);
+    HRESULT Init();
+    void Update();
+    void Uninit();
+    void Draw();
+
+private:
+    void SetParticle();        // パーティクルを出す処理
+    //==================
+    // メンバ変数
+    //==================
+    float m_fAngle;                // 角度
+    float m_fSpeed;                // 速度
+    int m_nLife;                   // 体力
+    static float m_fSize;
+};
+
+#endif
